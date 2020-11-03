2c86d1161e44484b05e3802f03ee92efdaa14e2a
Pablo Fernandez <pablofernandez.95@hotmail.com>
2020-11-03 17:02:40 -0300

    Mejoras en sitio, estilo + mapa

---
+++ public/js/map.js
@@ -0,0 +1,7 @@
+var mymap = L.map('main_map').setView([-34.5958528,-58.652389], 12);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(mymap);
+
+L.marker([-34.6037389,-58.3815704,15]).addTo(mymap);
